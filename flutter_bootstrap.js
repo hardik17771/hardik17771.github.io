cc0feff59f1da7b0894883ca8f299c326c43e260
hardik17771 <hardiksachdeva52@gmail.com>
2025-05-23 16:28:15 +0530

    next v1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1646036920"
+    serviceWorkerVersion: "423757408"
   }
 });
